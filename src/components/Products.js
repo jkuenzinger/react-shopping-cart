2beaaeb373b8af96eccfefe71b7abb5f41989fe8
jkuenzinger <jkuenzinger228@gmail.com>
2020-03-19 11:50:50 -0700

    brought the cart and product context into the product and shopping cart componetns and refactored from props to context inside these two components going to do navigation next

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,22 @@
-import React from 'react';
-
+import React, {useContext} from 'react';
+// need to import use context and product context here
+import {ProductContext} from '../Context/ProductContext';
 // Components
 import Product from './Product';
 
-const Products = props => {
+//Now that we ahave context we are going to remove props from this product component
+// I am also goign to add the functionallity to use Product Context
+const Products = () => {
+	const {products, addItem} = useContext(ProductContext);
 	return (
+
+// now that i have the context here I can refactor and remove props form the return of this function
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,18 +1,21 @@
-import React from 'react';
-
+import React, {useContext} from 'react';
+// importing use context and cart context liek w did with product context 
 // Components
+import {CartContext} from '../Contexts/CartContext';
 import Item from './ShoppingCartItem';
 
-const ShoppingCart = props => {
+// below i am declaring car as the useContext CartContext data I can then remove props from this componenet.
+const ShoppingCart = () => {
+	const cart = useContext(CartContext);
 	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
 
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
+			{cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
 
