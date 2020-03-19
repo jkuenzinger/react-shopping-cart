10baca6249ac16fa7b303733ce73e1b35785534c
jkuenzinger <jkuenzinger228@gmail.com>
2020-03-19 11:39:14 -0700

    created the Product Context and Cart Context in their own files imported them into app.js then added the fucntionality for AddItem to work with the new context, also wrapped the component trees in both provider tags to pass in the data

--- src/App.js
+++ src/App.js
@@ -6,17 +6,27 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+// import the ProductContext and CartContext so that I can have the data here
+import {ProductContext} from './Contexts/ProductContext.js';
+import {CartContext} from './Contexts/CartContext.js';
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+
+		/// spreading the current cart state and adding the new item to the end of the array
+		setCart([...cart,item]);
 	};
 
+
+	// I know ned to wrap the product and cart context around the return where i need to pass the 
+	//data from the created contexts
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{products, addItem}}>
+			<CartContext.Provider value={{cart}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
@@ -27,6 +37,8 @@ function App() {
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
